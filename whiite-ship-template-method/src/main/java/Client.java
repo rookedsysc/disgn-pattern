9a00348c165297a59f6a727524a75f4b1d65658b
rookedsysc <rookedsysc36@gmail.com>
2024-05-03 01:08:24 +0900

    템플릿 콜백 메서드 패턴

--- whiite-ship-template-method/src/main/java/Client.java
+++ whiite-ship-template-method/src/main/java/Client.java
@@ -3,6 +3,9 @@
 public class Client {
     public static void main(String[] args) {
         FileProcessor fileProcessor = new FileProcessor("number.txt");
-        System.out.println(fileProcessor.process());
+        System.out.println(fileProcessor.process((a,b) -> {
+            return a + b;
+        }));
+//        System.out.println(fileProcessor.process(Integer::sum));
     }
 }
--- whiite-ship-template-method/src/main/java/FileProcessor.java
+++ whiite-ship-template-method/src/main/java/FileProcessor.java
@@ -2,26 +2,24 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public abstract class FileProcessor {
+public class FileProcessor {
     private String path;
 
     public FileProcessor(String path) {
         this.path = path;
     }
 
-    public int process() {
+    // call-back을 parameter로 받아서 처리
+    public int process(Operator opreator) {
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
             int result = 0;
             String line = null;
             while ((line = reader.readLine()) != null) {
-                result = getResult(result, Integer.parseInt(line));
+                result = opreator.getResult(result, Integer.parseInt(line));
             }
             return result;
         } catch (IOException e) {
             throw new IllegalArgumentException(path + "에 해당하는 파일이 없습니다.", e);
         }
     }
-
-    // protected로 구현클래스에서 접근 가능하게 설정
-    protected abstract int getResult(int result, int line);
 }
