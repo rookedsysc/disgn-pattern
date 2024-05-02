303103e9c3838afc0164224eb4dc1c4be7074f49
rookedsysc <rookedsysc36@gmail.com>
2024-05-02 23:50:37 +0900

    Spring Aspect Around를 활용한 AOP

---
+++ white-ship-proxy/src/main/java/in_java/GameService.java
@@ -0,0 +1,10 @@
+package in_java;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameService {
+    public void startGame() {
+        System.out.println("Game started");
+    }
+}
