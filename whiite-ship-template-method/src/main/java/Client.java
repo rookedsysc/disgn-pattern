1736d1887826ac2fc7423abfa8e40d8288e66104
rookedsysc <rookedsysc36@gmail.com>
2024-05-03 01:16:06 +0900

    콜백으로 넘어갈 interface를 구현한 구현체 클래스를 주입

--- whiite-ship-template-method/src/main/java/Client.java
+++ whiite-ship-template-method/src/main/java/Client.java
@@ -3,9 +3,7 @@
 public class Client {
     public static void main(String[] args) {
         FileProcessor fileProcessor = new FileProcessor("number.txt");
-        System.out.println(fileProcessor.process((a,b) -> {
-            return a + b;
-        }));
+        System.out.println(fileProcessor.process(new Plus()));
 //        System.out.println(fileProcessor.process(Integer::sum));
     }
 }
