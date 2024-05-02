26c7d856f328b3d242d13d423fa86eb3a9cb3b82
rookedsysc <rookedsysc36@gmail.com>
2024-05-03 00:13:38 +0900

    요구사항 변경에 따라 모든 라인을 곱셈으로 하는 중복된 코드 추가

---
+++ whiite-ship-template-method/src/main/java/MultipleFileProcessor.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MultipleFileProcessor {
+    private String path;
+
+    public MultipleFileProcessor(String path) {
+        this.path = path;
+    }
+
+    public int process() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            int result = 1;
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                result *= Integer.parseInt(line);
+            }
+            return result;
+        } catch (IOException e) {
+            throw new IllegalArgumentException(path + "에 해당하는 파일이 없습니다.", e);
+        }
+    }
+}
